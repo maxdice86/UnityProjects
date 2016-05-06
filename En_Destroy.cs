9acca810232b8264e56061682f3987a410d3ae0d
maxdice86 <yaadlink@yahoo.com>
2016-05-06 05:48:26 -0400

    Added files via upload

---
+++ En_Destroy.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class En_Destroy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+
+		Destroy (gameObject, 10f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter( Collision player){
+	
+		if (player.gameObject.tag == "Player") {
+		
+			GameObject.Find("Canvas").GetComponentInChildren<Spawner> ().health -=20;
+
+			Debug.Log (player);
+		}
+	
+	}
+}
